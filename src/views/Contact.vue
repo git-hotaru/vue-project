c75bcbd63413e277afd070ff4c4bcf1be4a75a86
hotaru <79352982+hotaru-git@users.noreply.github.com>
2021-10-10 23:27:46 +0900

    typescript　プロジェクト

---
+++ src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="contact">
+        <h1>contact画面です</h1>
+    </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// port番号を変更
+
+module.exports = {
+    devServer: {
+        host: '0.0.0.0',
+        port: '8000',
+    }
+}
